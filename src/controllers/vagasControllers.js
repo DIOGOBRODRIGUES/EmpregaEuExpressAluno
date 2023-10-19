294b891496b122a075fdc648c020520a321ba9b8
DIOGOBRODRIGUES <diogo.borbar@gmail.com>
2023-10-19 11:30:02 -0300

    Incio  do desenvolvimento do controller

---
+++ src/controllers/vagasControllers.js
@@ -0,0 +1,17 @@
+//import do modelo de vaga criado  na camada models 
+import vaga from "../models/vagas.js";
+
+/*Criando nossa Classe lembrando que comecamos com letra maiuscula, a classe 
+não recebe parametros e não tem retorno*/
+
+class Vagas {
+
+    /* 
+     vamos criar um função assync pra conectar com banco 
+     Criar metodo de listarVagas e recebe dois parametros, o controlador
+     que vai manejar as requisicoes e respostas*/
+    static async listarVagas() 
+}
+
+//exportar clase para ser utilizadas em outras partes 
+export default Vagas;
